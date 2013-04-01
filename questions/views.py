a5809794fb87525b7d35b1c798d22982b04a75ef
Philip Cooksey <pcooksey@csumb.edu>
2013-03-31 19:53:12 -0700

    Changed the questions app
    Made it so one question comes at a time. The index function handles most
    of the requests now.

--- questions/views.py
+++ questions/views.py
@@ -4,13 +4,38 @@ from django.http import HttpResponse
 
 def index(request):
   questions = Question.objects.all()
-  context = {"questions": questions}
+  #Does the first page of the questions
+  if 'number' not in request.POST:
+	context = {"questions": questions, "number": 1 }
+	return render(request, "questions/index.html", context)
+  else:
+    number = int(request.POST['number'])
+	
+  #Getting the value for the answer	
+  answer = int(request.POST[str(number)])
+  
+  # Number 1 is the first question for Donor Database
+  if number == 1:
+	if answer == 1:
+	  generateDonorDatabase()
+	  return HttpResponse("Generate Donor Database")
+	else:
+	  context = {"questions": questions, "number": 2 }
+	  return render(request, "questions/index.html", context)
+	  
+  # Number 2 is the tracking data database
+  elif number == 2 and answer == 1:
+	  return HttpResponse("We have not finished this part! <br /> Work is still need.")
+	
+  return HttpResponse("We do not have the database you need!")
 
-  return render(request, "questions/index.html", context)
-
+def generateDonorDatabase():
+	return 0
+  
 def submit(request):
   questions = Question.objects.all()
 
   output = "<br />".join([request.POST[str(index.id)] for index in questions])
-
+  for index in questions:
+  	value = request.POST[str(index.id)]
   return HttpResponse(output)
