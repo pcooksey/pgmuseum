1bb08e4740333cdc444dac73e798330b19c2fdd4
cohansen <codyhansen92@gmail.com>
2013-04-02 15:28:27 -0500

    Added the final step in creating a tracking database

--- questions/views.py
+++ questions/views.py
@@ -7,7 +7,7 @@ def index(request):
   questions = Question.objects.all()
   # Does the first page of the questions
   if 'number' not in request.POST:
-    context = {"questions": questions, "number": 1, "radio": True }
+    context = { "questions": questions, "number": 1, "radio": True }
     return render(request, "questions/index.html", context)
   else:
     number = int(request.POST['number'])
@@ -21,19 +21,25 @@ def index(request):
       generateDonorDatabase()
       return HttpResponse("Generate Donor Database")
     else:
-      context = {"questions": questions, "number": 2, "radio": True }
+      context = { "questions": questions, "number": 2, "radio": True }
       return render(request, "questions/index.html", context)
   # Number 2 is the tracking data database
   elif number == 2 and answer == 1:
-    context = {"questions": questions, "number": 3, "numberInput": True }
+    context = { "questions": questions, "number": 3, "numberInput": True }
     return render(request, "questions/index.html", context)
-  
+  # Number 3 is getting the number of items in the database
+  elif number == 3:
+    numOfFields = int(request.POST[str(number)])
+    context = { "number": 4, "numOfFields": range(numOfFields) }
+    return render(request, "questions/index.html", context)
+  # Number 4 is generating the tracking database
+  elif number == 4:
+    generateTrackingDatabase()
+
   return HttpResponse("We do not have the database you need!")
 
 def generateDonorDatabase():
   return 0
 
-def followUpQuestions():
-  
-
+def generateTrackingDatabase():
   return 0
