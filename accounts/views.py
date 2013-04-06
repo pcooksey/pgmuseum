609444770778f723bf55e7be51021762801fb8f1
cohansen <codyhansen92@gmail.com>
2013-04-06 15:10:03 -0500

    Registration now works

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django import forms
+from django.contrib.auth.models import User
 
 def index(request):
   context = {}
-
-  return render(request, "accounts/index.html", context)
+  
+  if "register" in request.POST:
+    return redirect("/accounts/register/")
+  elif "signin" in request.POST:
+    return HttpResponse("login")
+  else:
+    return render(request, "accounts/index.html", context)
 
 def register(request):
   context = {}
+
+  if "createAccount" in request.POST:
+    email = request.POST["email"]
+    password = request.POST["password"]
+    confirmPassword = request.POST["confirmPassword"]
+    firstName = request.POST["firstName"]
+    lastName = request.POST["lastName"]
+
+    if email and password and firstName and lastName and password == confirmPassword:
+      object, created = User.objects.get_or_create(username = email)
+
+      if created:
+        object.email = email
+        object.first_name = firstName
+        object.last_name = lastName
+        object.set_password(password)
+        object.save()
+
+        return redirect("/accounts/")
+      else:
+        return errorMessage(request, "User already exists!")
+    elif password != confirmPassword:
+      return errorMessage(request, "Your passwords do not match!")
+    else:
+      return errorMessage(request, "Please enter information in every field!")
+  else:
+    return render(request, "accounts/register.html", context)
+
+def errorMessage(request, error):
+  context = {"error": error}
 
   return render(request, "accounts/register.html", context)
