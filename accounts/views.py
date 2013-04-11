d4eb84e44264d5c5bfa9f2d0eb81180dc42cda89
root <codyhansen92@gmail.com>
2013-04-10 20:35:53 -0500

    Fixed some tabbing errors and refactored some code

--- accounts/views.py
+++ accounts/views.py
@@ -11,32 +11,33 @@ def index(request):
   if "register" in request.POST:
     return redirect("/accounts/register/")
   elif "signin" in request.POST:
-	if "email" in request.POST and "password" in request.POST:
-		email = request.POST["email"]
-		password = request.POST["password"]
-		user = authenticate(username = email, password = password)
+    if "email" in request.POST and "password" in request.POST:
+      email = request.POST["email"]
+      password = request.POST["password"]
+      user = authenticate(username = email, password = password)
 
-		if user is not None:
-		  if user.is_active:
-			login(request, user)
-			return redirect("/questions/")
-		  else:
-			return render(request, "accounts/index.html", { "error": "Your account is disabled!" })
-		else:
-		   return render(request, "accounts/index.html", { "error": "Incorrect username or password!" })
-	else:
-		return render(request, "accounts/index.html", { "error": "Incorrect username or password!" })
+      if user is not None:
+        if user.is_active:
+          login(request, user)
+
+          return redirect("/questions/")
+        else:
+          return render(request, "accounts/index.html", { "error": "Your account is disabled!" })
+      else:
+        return render(request, "accounts/index.html", { "error": "Incorrect username or password!" })
+    else:
+      return render(request, "accounts/index.html", { "error": "Incorrect username or password!" })
   else:
     return render(request, "accounts/index.html", context)
 
 def register(request):
   if "createAccount" in request.POST:
-	#Below should be refractored into a function to decrease code
-    email = request.POST["email"] if "email" in request.POST else ""
-    password = request.POST["password"] if "password" in request.POST else ""
-    confirmPassword = request.POST["confirmPassword"] if "confirmPassword" in request.POST else ""
-    firstName = request.POST["firstName"] if "firstName" in request.POST else ""
-    lastName = request.POST["lastName"] if "lastName" in request.POST else ""
+  #Below should be refractored into a function to decrease code
+    email = verifyPost(request, "email")
+    password = verifyPost(request, "password")
+    confirmPassword = verifyPost(request, "confirmPassword")
+    firstName = verifyPost(request, "firstName")
+    lastName = verifyPost(request, "lastName")
 
     if email and password and firstName and lastName and password == confirmPassword:
       object, created = User.objects.get_or_create(username = email)
@@ -62,3 +63,9 @@ def errorMessage(request, error):
   context = { "error": error }
 
   return render(request, "accounts/register.html", context)
+
+def verifyPost(request, string):
+  if string in request.POST:
+    return request.POST[string]
+
+  return ""
