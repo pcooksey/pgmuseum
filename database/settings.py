6942091209b7d7cb849ec30e3eb1a14e2444cde8
Philip Cooksey <pcooksey@csumb.edu>
2013-04-14 15:04:06 -0700

    Add allowed host

--- database/settings.py
+++ database/settings.py
@@ -4,7 +4,7 @@ import os.path
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -165,8 +165,10 @@ LOGGING = {
 }
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['http://database-in-a-box.herokuapp.com/']
