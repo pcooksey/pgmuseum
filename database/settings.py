ea63714ab59ee784486ab90f443cac6b50dd3bf4
Philip Cooksey <pcooksey@csumb.edu>
2013-04-14 15:07:34 -0700

    Trying allowed host

--- database/settings.py
+++ database/settings.py
@@ -171,4 +171,4 @@ LOGGING = {
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['http://database-in-a-box.herokuapp.com/']
+ALLOWED_HOSTS = ['database-in-a-box.herokuapp.com/']
