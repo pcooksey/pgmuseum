0344ee170904f5e8e1404a115cd1e00bfaa01918
Philip Cooksey <pcooksey@csumb.edu>
2013-04-30 20:26:57 -0700

    Changing url

--- database/settings.py
+++ database/settings.py
@@ -171,4 +171,4 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['database-in-a-box.herokuapp.com']
+ALLOWED_HOSTS = ['pgmuseum.herokuapp.com']
