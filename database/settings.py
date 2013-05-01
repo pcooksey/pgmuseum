09375262a9c43a858398c445eb8add5e01eea63e
Philip Cooksey <pcooksey@csumb.edu>
2013-05-01 09:32:28 -0700

    Altered model

--- database/settings.py
+++ database/settings.py
@@ -130,7 +130,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'questions',
     'accounts',
-    'donor'
+    'donor',
+	'datasheet'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -165,8 +166,8 @@ LOGGING = {
 }
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
