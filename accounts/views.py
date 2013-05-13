26be64b91b2fbfa949c24fcecadb84a030541490
Philip Cooksey <pcooksey@csumb.edu>
2013-05-13 12:48:27 -0700

    Added access code

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from donor.models import Donor, Donation
-from donor.models import AccessCode
+from accounts.models import AccessCode
 
 def index(request):
   context = {}
@@ -34,38 +34,38 @@ def index(request):
     return render(request, "accounts/index.html", context)
 
 def register(request):
-  if "createAccount" in request.POST:
-	#Below should be refractored into a function to decrease code
-	email = verifyPost(request, "email")
-	password = verifyPost(request, "password")
-	confirmPassword = verifyPost(request, "confirmPassword")
-	firstName = verifyPost(request, "firstName")
-	lastName = verifyPost(request, "lastName")
-	accessCode = verifyPost(request, "accessCode")
-	try:
-		code = AccessCode.objects.get(code = accessCode)
-	except ObjectDoesNotExist:
-		return errorMessage(request, "Invalid access code")
+	if "createAccount" in request.POST:
+		#Below should be refractored into a function to decrease code
+		email = verifyPost(request, "email")
+		password = verifyPost(request, "password")
+		confirmPassword = verifyPost(request, "confirmPassword")
+		firstName = verifyPost(request, "firstName")
+		lastName = verifyPost(request, "lastName")
+		accessCode = verifyPost(request, "accessCode")
+		try:
+			code = AccessCode.objects.get(code = accessCode)
+		except ObjectDoesNotExist:
+			return errorMessage(request, "Invalid access code")
 
-    if email and password and firstName and lastName and password == confirmPassword:
-      object, created = User.objects.get_or_create(username = email)
+		if email and password and firstName and lastName and password == confirmPassword:
+			object, created = User.objects.get_or_create(username = email)
 
-      if created:
-        object.email = email
-        object.first_name = firstName
-        object.last_name = lastName
-        object.set_password(password)
-        object.save()
+			if created:
+				object.email = email
+				object.first_name = firstName
+				object.last_name = lastName
+				object.set_password(password)
+				object.save()
 
-        return redirect("/accounts/")
-      else:
-        return errorMessage(request, "User already exists!")
-    elif password != confirmPassword:
-      return errorMessage(request, "Your passwords do not match!")
-    else:
-      return errorMessage(request, "Please enter information in every field!")
-  else:
-    return render(request, "accounts/register.html", {})
+				return redirect("/accounts/")
+			else:
+				return errorMessage(request, "User already exists!")
+		elif password != confirmPassword:
+			return errorMessage(request, "Your passwords do not match!")
+		else:
+			return errorMessage(request, "Please enter information in every field!")
+	else:
+		return render(request, "accounts/register.html", {})
 
 def errorMessage(request, error):
   context = { "error": error }
