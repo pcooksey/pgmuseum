ce9630ef5ed6b586b0dc73d1a94f2fd07e40ebed
Philip Cooksey <pcooksey@csumb.edu>
2013-09-06 17:49:06 -0700

    Trying to install South

--- database/settings.py
+++ database/settings.py
@@ -125,7 +125,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'accounts',
-	'datasheet'
+	'datasheet',
+	'south'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
