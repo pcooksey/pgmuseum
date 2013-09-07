59874347f6335246f9a7bc8d1678c5d69dec0792
Philip Cooksey <pcooksey@csumb.edu>
2013-09-06 19:21:58 -0700

    Adding cascading delete

--- accounts/views.py
+++ accounts/views.py
@@ -233,7 +233,13 @@ def delete(request):
     basic = Basic.objects.get(id = request.POST["delete"])
 
     if request.user.is_staff == True:
-      basic.delete()
+	  basic.notes.delete()
+	  basic.weather.delete()
+	  basic.observers.delete()
+	  basic.exploration_time.delete()
+	  basic.count_time.delete()
+	  basic.butterflies_observed.delete()
+	  basic.delete()
  
     return redirect("/accounts/home/")
   else:
